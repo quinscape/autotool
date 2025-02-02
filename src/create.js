60557c61b1237984a9a5c497ffaf3ef532857ef5
Sven Helmberger <fforw@gmx.de>
2025-02-02 14:40:39 +0100

    Implement create command
    Copy internal new domain file to target only if that target does not already exist

--- src/create.js
+++ src/create.js
@@ -1,5 +1,20 @@
-module.exports = function create(path)
+const fs = require("fs")
+const path = require("path")
+const shelljs = require("shelljs")
+module.exports = function create(schemaPath)
 {
-    console.log("CREATE")
+    if (fs.existsSync(schemaPath))
+    {
+        console.error("Cannot create schema" + schemaPath + ": File does already exist.")
+    }
 
+    return new Promise(
+        resolve => {
+            shelljs.cp(
+                path.resolve(__dirname, "../domain/new.graphql"),
+                schemaPath
+            )
+            resolve()
+        }
+    )
 }

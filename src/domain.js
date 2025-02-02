898c1a7d09fef3ff4d89ab1d2fe3f0cedfd86000
Sven Helmberger <fforw@gmx.de>
2025-02-02 17:03:50 +0100

    Domain: Pretty print if requested

--- src/domain.js
+++ src/domain.js
@@ -5,7 +5,7 @@ module.exports = function db(path,opts)
     return loadSchema(path,opts)
         .then(
             (schema) => {
-                console.log(JSON.stringify(schema))
+                console.log(JSON.stringify(schema, null, opts.prettyJSON ? 4 : 0))
             })
 }
 

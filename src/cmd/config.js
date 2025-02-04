492a578d49af9913b7c13642e1411d886d88817c
Sven Helmberger <fforw@gmx.de>
2025-02-04 14:01:31 +0100

    Implement config command
    Implement config command to output the Java calls to put into the
    GraphQLConfiguration to create the relation field configuration
    expressed in the boostrap schema

--- src/cmd/config.js
+++ src/cmd/config.js
@@ -7,7 +7,37 @@ module.exports = function config(path,opts)
         .then(
             (schema) => {
 
-                return "TODO: implement config"
+                //console.log(JSON.stringify(schema, null, 4))
+
+                let out = ""
+                const { types, linkTables } = schema
+                for (let i = 0; i < types.length; i++)
+                {
+                    const {name, refs, backRefs} = types[i]
+
+                    for (let j = 0; j < refs.length; j++)
+                    {
+                        const ref = refs[j]
+                        const backRef = backRefs.find(b => b.sourceType === name && b.sourceField === ref.name)
+                        //console.log({backRef, name, field: ref.name, backRefs})
+
+                        out += `.configureRelation(${name.toUpperCase()}.${ref.name.toUpperCase()}, SourceField.OBJECT_AND_SCALAR, TargetField.${backRef != null ? "MANY" : "NONE"}, "${ref.name}", ${backRef != null ? '"' + backRef.name + '"' : "null"})${EOL}`
+                    }
+                }
+
+                for (let i = 0; i < linkTables.length; i++)
+                {
+                    const { name, refs } = linkTables[i]
+                    const targetType = types.find( t => t.name === refs[1].type)
+                    const backRef = targetType.backRefs.find(b => b.type === refs[1].type && b.name === refs[1].name)
+
+                    out += `.configureRelation(${name.toUpperCase()}.${(refs[1].type + "_id").toUpperCase()}, SourceField.OBJECT_AND_SCALAR, TargetField.MANY, "${refs[1].type}", ${backRef != null ? '"' + backRef.name + '"' : "null"})${EOL}`
+                    out += `.configureRelation(${name.toUpperCase()}.${(refs[0].type + "_id").toUpperCase()}, SourceField.OBJECT_AND_SCALAR, TargetField.MANY, "${refs[0].type}", ${refs[0].name})${EOL}`
+
+                }
+
+
+                return out;
             })
 }
 
